cbc3d0d169bd0a750d60b51d2ff5eed54e30ef9f
choppen <s1146602@student.windesheim.nl>
2019-11-18 13:36:12 +0100

    Nu is het een functie

--- product.php
+++ product.php
@@ -1,17 +1,24 @@
-'
-<div class="card mb-3" style="max-width: 80%;">
-<div class="row no-gutters">
-    <div class="col-md-4">
-    <img src='.$Img.' class="card-img" style="object-fit: contain; max-height: 200px;">
+<?php
+
+function ProductResult($Img, $Name, $Beschrijving, $Prijs){
+    print('
+    <div class="card mb-3" style="max-width: 80%;">
+        <div class="row no-gutters">
+            <div class="col-md-4">
+                <img src='.$Img.' class="card-img" style="object-fit: contain; max-height: 200px;">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">'. $Name . '</h5>
+                    <p class="card-text">'.$Beschrijving.'</p>
+                    <p class="card-text"> €'.$Prijs.'</p>
+                    <p class="card-text"><small class="text-muted">Vooraad: '. $Vooraad .' STK</small></p>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col-md-8">
-    <div class="card-body">
-        <h5 class="card-title">'. $Name . '</h5>
-        <p class="card-text">'.$Beschrijving.'</p>
-        <p class="card-text"> €'.$Prijs.'</p>
-        <p class="card-text"><small class="text-muted">Vooraad: '. $Vooraad .' STK</small></p>
-    </div>
-    </div>
-</div>
-</div>
-'
+    '
+    );
+}
+
+?>
