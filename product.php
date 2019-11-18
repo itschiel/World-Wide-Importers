9d3bb9487b0a709dfb00570415c6c02087eb3968
choppen <57718912+choppen@users.noreply.github.com>
2019-11-18 13:40:15 +0100

    ProductResult.php

--- product.php
+++ product.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 function ProductResult($Img, $Name, $Beschrijving, $Prijs){
@@ -23,20 +22,3 @@ function ProductResult($Img, $Name, $Beschrijving, $Prijs){
 }
 
 ?>
-=======
-<div class="card mb-3" style="max-width: 80%;">
-<div class="row no-gutters">
-    <div class="col-md-4">
-    <img src='.$Img.' class="card-img" style="object-fit: contain; max-height: 200px;">
-    </div>
-    <div class="col-md-8">
-    <div class="card-body">
-        <h5 class="card-title">'. $Name . '</h5>
-        <p class="card-text">'.$Beschrijving.'</p>
-        <p class="card-text"> €'.$Prijs.'</p>
-        <p class="card-text"><small class="text-muted">Vooraad: '. $Vooraad .' STK</small></p>
-    </div>
-    </div>
-</div>
-</div>
->>>>>>> 9aa73bc2b686216731ea54969e34367e0a6e4ca6
