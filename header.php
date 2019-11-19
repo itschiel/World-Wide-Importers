d3fedeac039b7ff8a99547124a5f12965fe84bc8
SietseBijlsma <s1142382@student.windesheim.nl>
2019-11-19 12:04:27 +0100

    mwe

--- header.php
+++ header.php
@@ -19,6 +19,11 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+<div class="jumbotron text-center" style="margin-bottom:0">
+    <h1>WideWorldImporters</h1>
+    <p>Header van pagina voorbeeld</p>
+</div>
+
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 <form class="form-inline" action="/action_page.php">
     <input class="form-control mr-sm-2" type="text" placeholder="Search">
