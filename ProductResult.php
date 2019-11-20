b699121dce600cc9317e2bf27a4adcd951133bab
choppen <s1146602@student.windesheim.nl>
2019-11-20 12:22:59 +0100

    Wel of geen foto functie ingebouwd + onodige functies verwijderd

--- ProductResult.php
+++ ProductResult.php
@@ -1,27 +1,5 @@
 <?php
 
-function ProductResult($Img, $Name, $Beschrijving, $Prijs, $Vooraad, $id){
-    print('
-        <a href="ProductPreview.php?id='. $id . '" class="card mb-3" style="max-width: 80%;">
-            <div class="row no-gutters">
-                <div class="col-md-4">
-                    <img src='.$Img.' class="card-img" style="object-fit: contain; max-height: 200px;">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">'. $Name . '</h5>
-                        <p class="card-text">'.$Beschrijving.'</p>
-                        <p class="card-text"> €'.$Prijs.'</p>
-                        <p class="card-text"><small class="text-muted">Vooraad: '. $Vooraad .' STK</small></p>
-                    </div>
-                </div>
-            </div>
-        </a>
-    '
-    );
-}
-
-
 function WeergevenProducten($Connection, $Query){
         
     $Result = mysqli_query($Connection, $Query);
@@ -35,39 +13,51 @@ function WeergevenProducten($Connection, $Query){
             $Prijs = $row['RecommendedRetailPrice'];
             $Vooraad = $row['QuantityOnHand'];
             $ID = $row['StockItemID'];
-
-            if (empty($row['Photo'])){
-                $Img = "https://cdn.cms-twdigitalassets.com/content/dam/blog-twitter/official/en_us/products/2017/rethinking-our-default-profile-photo/Avatar-Blog2-Round1.png.img.fullhd.medium.png";
-            } else {
-                $Img = $row['Photo'];
-            }
-
-            print('
-                <a href="ProductPreview.php?id='. $ID . '" class="card mb-3" style="max-width: 80%;">
-                    <div class="row no-gutters">
-                        <div class="col-md-4">');
             
             if (empty($row['Photo'])){
+
                 $Img = "https://cdn.cms-twdigitalassets.com/content/dam/blog-twitter/official/en_us/products/2017/rethinking-our-default-profile-photo/Avatar-Blog2-Round1.png.img.fullhd.medium.png";
-                print('<img src="'. $Img . '" class="card-img" style="object-fit: contain; max-height: 200px;">');
-            } else {
-                $Img = $row['Photo'];
-                print('<img src="data:image/jpeg;base64,'. base64_encode($Img).'" class="card-img" style="object-fit: contain; max-height: 200px;">');
-            }
-            
-            print('
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">'. $Name . '</h5>
-                                <p class="card-text">'.$Beschrijving.'</p>
-                                <p class="card-text"> €'.$Prijs.'</p>
-                                <p class="card-text"><small class="text-muted">Vooraad: '. $Vooraad .' STK</small></p>
+
+                print('
+                    <a href="ProductPreview.php?id='. $ID . '" class="card mb-3" style="max-width: 80%;">
+                        <div class="row no-gutters">
+                            <div class="col-md-4">
+                                <img src="'. $Img . '" class="card-img" style="object-fit: contain; max-height: 200px;">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">'. $Name . '</h5>
+                                    <p class="card-text">'.$Beschrijving.'</p>
+                                    <p class="card-text"> €'.$Prijs.'</p>
+                                    <p class="card-text"><small class="text-muted">Vooraad: '. $Vooraad .' STK</small></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-    ');
+                    </a>
+                ');
+
+            } else {
+
+                $Img = $row['Photo'];
+
+                print('
+                    <a href="ProductPreview.php?id='. $ID . '" class="card mb-3" style="max-width: 80%;">
+                        <div class="row no-gutters">
+                            <div class="col-md-4">
+                                <img src="data:image/jpeg;base64,'. base64_encode($Img).'" class="card-img" style="object-fit: contain; max-height: 200px;">     
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">'. $Name . '</h5>
+                                    <p class="card-text">'.$Beschrijving.'</p>
+                                    <p class="card-text"> €'.$Prijs.'</p>
+                                    <p class="card-text"><small class="text-muted">Vooraad: '. $Vooraad .' STK</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                ');
+            }
         }
     } else {
         print ("Er zijn geen resultaten gevonden.");
