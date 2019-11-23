e14137c99d2c0b003c60cd4af338575102167bdb
choppen <s1146602@student.windesheim.nl>
2019-11-23 12:47:17 +0100

    naamgeving aangepast naar lowerCamelCase

--- ProductPage.php
+++ ProductPage.php
@@ -1,121 +1,124 @@
 <!doctype html>
 <html lang="en">
-<head>
 
-    <title>WorldWideImporters</title>
+    <head>
 
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>WorldWideImporters</title>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Functie Includes -->
-    <?php include 'Functions/dbconnections.php'; ?>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-</head>
-<body>
+        <!-- Functie Includes -->
+        <?php include 'Functions/dbconnections.php'; ?>
 
-<!-- Voegt de Header to aan de pagina -->
-<?php include 'Includes/header.php';  ?>
+    </head>
 
+    <body>
 
-
-<?php
-
-// Variabeleid haalt het id van het gezoken product  uit de url
-$variabeleid = $_GET['id'];
-
-//$Result houd de waarde die de db terug stuurd aan de hand van de onderstaande query
-$sql = ("SELECT s.StockItemName, s.RecommendedRetailPrice, s.MarketingComments, s.Photo, s.SearchDetails, h.QuantityOnHand
-    FROM stockitems s
-    JOIN stockitemholdings h ON s.StockItemID = h.StockItemID
-    WHERE s.StockItemID = $variabeleid
-    ");
-
-$result = dbConnectionRoot($sql); // dbConnectionRoot staat onder (Functions/dbconnections.php)
-$resultcheck = mysqli_num_rows($result);
+        <!-- Voegt de Header to aan de pagina -->
+        <?php include 'Includes/header.php';  ?>
 
 
-// Onderstaande if statement chekct of de db daadwerkelijk een record heeft terug gestuurd
-if($resultcheck > 0){
-    // voor elke record in result wordt het onderstaande uitgevoerd
-    while($row = mysqli_fetch_assoc($result)){
 
-        // onderstaande if else statement checkt of er een foto bij het product zit zo niet wordt de deafult image geladen
-        if (empty($row['Photo'])) {
+        <?php
 
-            $img_path = ("img/defaultproduct.jpg");
-            $img_binary = fread(fopen($img_path, "r"), filesize($img_path));
-            $img = base64_encode($img_binary);
-        
-        } else {
-            $img = base64_encode($row["Photo"]);
-        }
-        
-        // onderstaande print plaatst de benodigde html op de pagina
-        print("
-            <div class=\"container-fluid\">
-                <div class=\"row\">
-                    <div class=\"col-lg\">
-                        <div class=\"container\" style=\"margin-top:30px\">
+            // Variabeleid haalt het id van het gezoken product  uit de url
+            $productID = $_GET['id'];
+
+            //$Result houd de waarde die de db terug stuurd aan de hand van de onderstaande query
+            $query = ("SELECT s.StockItemName, s.RecommendedRetailPrice, s.MarketingComments, s.Photo, s.SearchDetails, h.QuantityOnHand
+                FROM stockitems s
+                JOIN stockitemholdings h ON s.StockItemID = h.StockItemID
+                WHERE s.StockItemID = $productID
+                ");
+
+            $result = mysqli_query(dbConnectionRoot(), $query) // dbConnectionRoot staat onder (Functions/dbconnections.php)
+            $resultCheck = mysqli_num_rows($result);
+
+
+            // Onderstaande if statement chekct of de db daadwerkelijk een record heeft terug gestuurd
+            if($resultCheck > 0){
+                // voor elke record in result wordt het onderstaande uitgevoerd
+                while($row = mysqli_fetch_assoc($result)){
+
+                    // onderstaande if else statement checkt of er een foto bij het product zit zo niet wordt de deafult image geladen
+                    if (empty($row['Photo'])) {
+
+                        $imgPath = ("Img/defaultProduct.jpg");
+                        $imgBinary = fread(fopen($imgPath, "r"), filesize($imgPath));
+                        $img = base64_encode($imgBinary);
+                    
+                    } else {
+                        $img = base64_encode($row["Photo"]);
+                    }
+                    
+                    // onderstaande print plaatst de benodigde html op de pagina
+                    print("
+                        <div class=\"container-fluid\">
                             <div class=\"row\">
-                                <div class=\"col-sm\">
-                                    <h2>PRODUCTNAAM: ". $row['StockItemName'] ."</h2>
-                                    <h6>Review systeem in sterren moet hier komen</h6>
-                                    <div class=\"row\">
-                                        <div class=\"col-md-4\">
-                                            <div class=\"thumbnail\">
-                                                <a href=\"img/defaultproduct.jpg\">
-                                                    <img src=\"data:image/jpeg;base64, $img \" alt=\"Lights\" style=\"width:100%\">
-                                                    <div class=\"caption\">
+                                <div class=\"col-lg\">
+                                    <div class=\"container\" style=\"margin-top:30px\">
+                                        <div class=\"row\">
+                                            <div class=\"col-sm\">
+                                                <h2>PRODUCTNAAM: ". $row['StockItemName'] ."</h2>
+                                                <h6>Review systeem in sterren moet hier komen</h6>
+                                                <div class=\"row\">
+                                                    <div class=\"col-md-4\">
+                                                        <div class=\"thumbnail\">
+                                                            <a href=\"img/defaultproduct.jpg\">
+                                                                <img src=\"data:image/jpeg;base64, $img \" alt=\"Lights\" style=\"width:100%\">
+                                                                <div class=\"caption\">
+                                                                </div>
+                                                            </a>
+                                                        </div>
                                                     </div>
-                                                </a>
+                                                    <div class=\"col-md-4\">
+                                                        <div class=\"thumbnail\">
+                                                            <a href=\"img/defaultproduct.jpg\">
+                                                            <img src=\"data:image/jpeg;base64, $img \" alt=\"Lights\" style=\"width:100%\">
+                                                                <div class=\"caption\">
+                                                                </div>
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <br>
+                                                <p class=\"font-weight-bold\">Productinformatie: </p>
+                                                <p>" . $row['MarketingComments']. "</p>
+                                                <p class=\"font-weight-bold\">Productbeschrijving: </p>
+                                                <p>". $row['SearchDetails'] ."</p>
+                                                <p class=\"font-weight-bold\">Video: </p>
+                                                <p>Link naar het filmmateriaal van het product: <a href=\"video.html\">Klik hier</a> </p>
                                             </div>
-                                        </div>
-                                        <div class=\"col-md-4\">
-                                            <div class=\"thumbnail\">
-                                                <a href=\"img/defaultproduct.jpg\">
-                                                <img src=\"data:image/jpeg;base64, $img \" alt=\"Lights\" style=\"width:100%\">
-                                                    <div class=\"caption\">
-                                                    </div>
-                                                </a>
+                                            <div class=\"card border-dark mb-3\" style=\"max-width: 18rem\">
+                                                <div class=\"card-header\">
+                                                <h4 class=\"my-0 font-weight-normal\">Prijs</h4>
+                                            </div>
+                                            <div class=\"card-body\">
+                                                <h1 class=\"card-title pricing-card-title\">$". $row['RecommendedRetailPrice'] ."</h1>
+                                                <ul class=\"list-unstyled mt-3 mb-4\">
+                                                    <li>". $row['QuantityOnHand'] ." stuk(s) voorradig</li>
+                                                    <br><br><br><br><br><br>
+                                                </ul>
+                                                <button type=\"button\" class=\"btn btn-lg btn-block btn-outline-primary\">In winkelmand</button>
                                             </div>
                                         </div>
                                     </div>
-                                    <br>
-                                    <p class=\"font-weight-bold\">Productinformatie: </p>
-                                    <p>" . $row['MarketingComments']. "</p>
-                                    <p class=\"font-weight-bold\">Productbeschrijving: </p>
-                                    <p>". $row['SearchDetails'] ."</p>
-                                    <p class=\"font-weight-bold\">Video: </p>
-                                    <p>Link naar het filmmateriaal van het product: <a href=\"video.html\">Klik hier</a> </p>
-                                </div>
-                                <div class=\"card border-dark mb-3\" style=\"max-width: 18rem\">
-                                    <div class=\"card-header\">
-                                    <h4 class=\"my-0 font-weight-normal\">Prijs</h4>
-                                </div>
-                                <div class=\"card-body\">
-                                    <h1 class=\"card-title pricing-card-title\">$". $row['RecommendedRetailPrice'] ."</h1>
-                                    <ul class=\"list-unstyled mt-3 mb-4\">
-                                        <li>". $row['QuantityOnHand'] ." stuk(s) voorradig</li>
-                                        <br><br><br><br><br><br>
-                                    </ul>
-                                    <button type=\"button\" class=\"btn btn-lg btn-block btn-outline-primary\">In winkelmand</button>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-    ");
-    }
-}
-?>
+                ");
+                }
+            }
+        ?>
 
-<!-- Voegt de Footer to aan de pagina -->
-<?php include 'Includes/footer.php';  ?>
+        <!-- Voegt de Footer to aan de pagina -->
+        <?php include 'Includes/footer.php';  ?>
 
-</body>
+    </body>
+
 </html>
