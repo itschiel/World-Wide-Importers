fab0f5eb52091da0e2f6e3306f7b7dcfdfe69120
choppen <57718912+choppen@users.noreply.github.com>
2019-12-04 22:50:27 +0100

    update header

--- login.php
+++ login.php
@@ -3,26 +3,29 @@ include "Includes/Header.php";
 ?>
 
 <div class="container">
-    <h2>Login</h2>
-    <form action="loginfunction.php" method="post">
-        <div class="form-group">
-            <input name="email" type="email" class="form-control" placeholder="E-mailadres">
-        </div>
-        <div class="form-group">
-            <input name="password" type="password" class="form-control" placeholder="Wachtwoord">
-        </div>
-        <div class="form-group">
-            <div class="row">
-                <div class="col-6">
-                    <a href="www.google.nl">Wachtwoord vergeten</a>
+    <div class="row">
+        <div class="col">
+            <h2>Login</h2>
+            <form action="loginfunction.php" method="post">
+                <div class="form-group">
+                    <input name="email" type="email" class="form-control" placeholder="E-mailadres">
                 </div>
-                <div class="col-6">
-                    <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                <div class="form-group">
+                    <input name="password" type="password" class="form-control" placeholder="Wachtwoord">
                 </div>
-            </div>
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-6">
+                            <a href="www.google.nl">Wachtwoord vergeten</a>
+                        </div>
+                        <div class="col-6">
+                            <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                        </div>
+                    </div>
+                </div>  
+            </form>
         </div>
-    
-    </form>
+    </div>
 </div>
 
 <!-- Registratie knop -->
