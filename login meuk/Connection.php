03056d0d3a4a52d16ae73cf9b14fc131d3d19403
choppen <57718912+choppen@users.noreply.github.com>
2019-12-05 13:17:50 +0100

    overbodige login code

--- login meuk/Connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$serverName = "localhost";
-$user = "root";
-$password = "";
-$databaseName = "logintest";
-$connection = mysqli_connect($serverName, $user, $password, $databaseName);
-if(!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-}
