225dc0229a836e3d4f9abdf3d8cb5c749d77e5f0
choppen <57718912+choppen@users.noreply.github.com>
2019-12-06 12:08:44 +0100

    sql code misc

---
+++ misc/Image_table_code.sql
@@ -0,0 +1,7 @@
+CREATE TABLE productImages (
+    fotoID int NOT NULL,
+    productID int NOT NULL,
+    foto LONGBLOB,
+    PRIMARY KEY (fotoID),
+    FOREIGN KEY (productID) REFERENCES stockitems(StockItemID)
+);
