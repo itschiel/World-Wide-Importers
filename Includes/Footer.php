61b83b34e14655a1b88c5b55d0d38d43e1dc3b8a
RBiemans <tinusbiemans@gmail.com>
2019-12-10 19:19:12 +0100

    t

--- Includes/Footer.php
+++ Includes/Footer.php
@@ -26,7 +26,7 @@
                         <a class="nav-link" href="#">www.info@wideworldimporters.com</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">06........</a>
+                        <a class="nav-link" href="#">06-telefoonnummer</a>
                     </li>
                 </ul>
             </div>        
