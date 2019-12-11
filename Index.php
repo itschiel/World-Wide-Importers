930585d9935e6051c41525ca2cf01e53f4a9c208
RBiemans <tinusbiemans@gmail.com>
2019-12-11 14:43:04 +0100

    homepagina

--- Index.php
+++ Index.php
@@ -9,6 +9,13 @@
 <?php include 'Includes/Header.php';?>
 
 
+    <div class="container">
+        <div class="col mx-auto">
+
+            
+
+        </div>
+    </div>
 
 
 <!-- Voegt de Footer to aan de pagina -->
