d7148590a050055ff829dd919c1f1da1a0de8237
Enes Özdemir <s1143071@student.windesheim.nl>
2019-12-13 13:26:02 +0100

    End

---
+++ End.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+    <title>WideWorldImporters</title>
+
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <!-- Functie Includes -->
+        <?php include_once 'Functions/DBConnections.php'; ?>
+</head>
+
+<body>
+
+<!-- Voegt de Header to aan de pagina -->
+<?php include 'Includes/Header.php';?>
+
+    <div class="card w-50">
+    <div class="card-body">
+    <h5 class="card-title">Bestelling afronden</h5>
+    <p class="card-text">Uw bestelling is geplaatst.<br>Bedankt voor u bestelling.<br>U ontvangt een bevestigingsmail van uw bestelling.</p>
+    <a href="Index.php" class="btn btn-primary">Ga verder met winkelen</a>
+  </div>
+</div>
+
+
+
+<?php include 'Includes/Footer.php';?>
+</body>
+</html>
