83a6597919d45fb9324290e00ab140d7e5ce72a8
choppen <57718912+choppen@users.noreply.github.com>
2019-12-16 11:16:29 +0100

    optimalisatie productResult fucntions

--- Functions/ProductResult.php
+++ Functions/ProductResult.php
@@ -21,7 +21,7 @@ function showProductCards($result){
 // onderstaande functie plaatst een proct tegel op basis van de aangeleverde array
 function productCard($row, $rate) {
 
-    $img = getPhoto($row['StockItemID']);
+    $img = getFirstPhoto($row['StockItemID']);
 
 
     // onderstaande print statement plaatst de benodigde html op de pagina
@@ -47,30 +47,36 @@ function productCard($row, $rate) {
     ');
 }
 
-function getPhoto($productID){
+function getFirstPhoto($productID){
 
+    // onderstaande query haalt de eerste foto van een product op
     $query = ("SELECT foto
         FROM productimages
         WHERE productid = $productID
         LIMIT 1
         ");
 
-    $result = mysqli_query(dbConnectionRoot(), $query); // dbConnectionRoot staat onder (Functions/dbconnections.php)
+    $connection = dbConnectionRoot();
+    $result = mysqli_query($connection, $query);
     $resultCheck = mysqli_num_rows($result);
 
     // onderstaande statement kijkt of er een img in de database staat zo niet wordt de dafault image geladen
     if ($resultCheck > 0) {
 
+        // dit is de foto uit de database
         $row = mysqli_fetch_assoc($result);
         $img = base64_encode($row["foto"]);
 
     } else {
 
+        // dit is de opgegeven deafult image
         $imgPath = ("img/defaultproduct.jpg");
         $imgBinary = fread(fopen($imgPath, "r"), filesize($imgPath));
         $img = base64_encode($imgBinary);
 
     }
+
+    mysqli_close($connection);
 
     return $img;
 
